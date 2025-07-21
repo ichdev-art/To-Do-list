0700235b23f00a7c724e34b3e90777c31a11b399
Ichem <ichem76610@hotmail.com>
2025-07-21 15:31:49 +0200

    creation to do list basique sans design react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./style.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const savedTasks = localStorage.getItem("myTasks");
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("myTasks", JSON.stringify(tasks));
+  }, [tasks]);
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  };
+  const handleDelete = (indexToRemove) => {
+    const updatedTasks = tasks.filter((_, index) => index !== indexToRemove);
+    setTasks(updatedTasks);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ma To-Do List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Ajouter une tâche"
+        />
+        <button type="submit">Ajouter</button>
+        
+      </form><ul>
+          {tasks.map((t, index) => (
+            <li key={index}>
+              {t}
+              <button onClick={() => handleDelete(index)}>❌</button>
+            </li>
+          ))}
+        </ul>
     </div>
   );
+  
 }
 
 export default App;
