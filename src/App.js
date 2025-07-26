4dc65bd7011ba567c49ffa9b373dd2ee13064fa9
Ichem <ichem76610@hotmail.com>
2025-07-26 14:04:21 +0200

    todo list + tri a l'ajout d'une tache + petit design ajout palette de couleur + effet ligne barré sur la tache accompli REACT JS

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (task.trim()) {
-      setTasks([...tasks, task]);
+      setTasks([...tasks, {text :task, done: false}]);
       setTask("");
     }
   };
@@ -30,7 +30,7 @@ function App() {
   };
   return (
     <div className="App">
-      <h1>Ma To-Do List</h1>
+      <h1 className="todo">To-Do List</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -40,10 +40,34 @@ function App() {
         />
         <button type="submit">Ajouter</button>
         
-      </form><ul>
+      </form>
+      
+      <h2 class="list">Liste des tâches</h2>
+      <section className="task-list">
+        {tasks.length === 0 && <p>Aucune tâche à afficher</p>}
+        <div className="task-count">
+          <p>Tâches terminées : {tasks.filter(t => t.done).length}</p>
+        </div>
+        <div className="task-count">
+          <p>Tâches en cours : {tasks.filter(t => !t.done).length}</p>
+        </div>
+        <div className="task-count">
+          <p>Tâches totales : {tasks.length}</p>
+        </div>
+        </section>        
+      <ul>
           {tasks.map((t, index) => (
             <li key={index}>
-              {t}
+              <input
+                type="checkbox"
+                checked={t.done}
+                onChange={() => {
+                  const updatedTasks = [...tasks];
+                  updatedTasks[index].done = !updatedTasks[index].done;
+                  setTasks(updatedTasks);
+                }}
+              />
+              {t.done ? "✔️" : "❌"} <span className={`task-text ${t.done ? "done" : ""}`}>{t.text}</span>
               <button onClick={() => handleDelete(index)}>❌</button>
             </li>
           ))}
